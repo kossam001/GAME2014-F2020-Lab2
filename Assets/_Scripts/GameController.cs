9cd039e619d6ef88868fa0ddc28332aaf2dba933
kossam001 <kossam001@gmail.com>
2020-09-29 11:25:21 -0400

    Added Lab2

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    //public TMP_Text SceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(370, -105);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349, -105);
+
+                //SceneLabel.text = "Landscape Left";
+                break;
+            case DeviceOrientation.LandscapeRight:
+
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248, -105);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-490, -105);
+
+                //SceneLabel.text = "Landscape Right";
+                break;
+            case DeviceOrientation.Portrait:
+
+                LivesLabel.rectTransform.anchoredPosition = new Vector2(248, -185);
+                ScoreLabel.rectTransform.anchoredPosition = new Vector2(-349, -185);
+
+                //SceneLabel.text = "Landscape Portrait";
+                break;
+            case DeviceOrientation.Unknown:
+                //SceneLabel.text = "Unknown Orientation";
+                break;
+        }
+    }
+}
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    //public TMP_Text SceneLabel;
+    public TMP_Text LivesLabel;
+    public TMP_Text ScoreLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        LivesLabel.rectTransform.anchoredPosition = new Vector3(370, -105, 0);
+        ScoreLabel.rectTransform.anchoredPosition = new Vector3(-349, -105, 0);
+    }
+}
